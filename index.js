3d03330db51891e67776332c8bc31ab9abd9b70a
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 15:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase();
+  return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
-function sayHiToGrandma(string){
-  if (string === string.toLowerCase)
-  return "I can't hear you!";
-} else {
-   return "I love you, too!" 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-  
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
